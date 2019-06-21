4e8b23f318ca4f061c4fb67ab7741be23fc2899c
Jengopockets <jengopockets@gmail.com>
2019-06-21 12:21:53 -0400

    Created actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
+export const ADDING = 'ADDING';
+export const DELETING = 'DELETING';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,41 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => (dispatch) => {
+  dispatch ({type: FETCHING})
+  axios
+  .get ('http://localhost:3333/smurfs')
+  .then(response => {
+    dispatch({type:SUCCESS, payload:response.data})
+  })
+  .catch(err => {
+    dispatch({type:ERROR, payload:err})
+  })
+}
+
+export const addSmurf = (index) => (dispatch) => {
+  dispatch ({type: ADDING})
+  axios
+  .post ('http://localhost:3333/smurfs' , index)
+  .then (response => {
+    dispatch({type: SUCCESS, payload : response.data});
+    return true
+  })
+  .catch ( err => {
+    dispatch({type: ERROR, payload : err})
+    return false
+  })
+}
+export const deleteSmurf = (index) => (dispatch) => {
+  dispatch ({type: DELETING})
+  axios
+  .delete (`http://localhost:3333/smurfs/${smurf}`)
+  .then (response => {
+    dispatch({type: SUCCESS, payload : response.data});
+    return true
+  })
+  .catch ( err => {
+    dispatch({type: ERROR, payload : err})
+    return false
+  })
+}
